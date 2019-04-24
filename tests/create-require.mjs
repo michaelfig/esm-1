9c8e5cd8f2d4ce8cdb587ea18a1341804248f4a4
Saleh <saleh@grasppe.com>
2019-04-24 17:44:20 -0400

    Update create-require.mjs

--- tests/create-require.mjs
+++ tests/create-require.mjs
@@ -1,5 +1,23 @@
 ﻿import {createRequireFromPath} from 'module';
+import {fileURLToPath} from 'url';
 
-const require = createRequireFromPath('.');
+const specifier = './package.json';
+const [scope, ...paths] = ['../../', '../', './'].map((path, index) =>
+  // '/index' fixes createRequireFromPath('…/') resolving against parent (why!)
+  fileURLToPath(new URL(index ? path.replace(/\/([?#].*?|)$/, '/index$1') : path, import.meta.url)),
+);
 
-console.log(require.resolve('./package.json'));
+for (const path of paths) {
+  console.group('\ncreateRequireFromPath(%o)\n  .resolve(%o)', path.replace(scope, '‹scope›/'), specifier);
+  try {
+    console.log(
+      `  => %o`,
+      createRequireFromPath(path)
+        .resolve(specifier)
+        .replace(scope, '‹scope›/'),
+    );
+  } catch (exception) {
+    console.warn(`  => %s`, `${exception}`.split('\n', 1)[0]);
+  }
+  console.groupEnd();
+}
