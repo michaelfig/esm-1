0bcd4c72bf537be9ae4d43b5dacb2410169b585d
Saleh <saleh@grasppe.com>
2019-04-24 17:50:19 -0400

    Update create-require.mjs

--- tests/create-require.mjs
+++ tests/create-require.mjs
@@ -3,21 +3,30 @@ import {fileURLToPath} from 'url';
 
 const specifier = './package.json';
 const [scope, ...paths] = ['../../', '../', './'].map((path, index) =>
-  // '/index' fixes createRequireFromPath('…/') resolving against parent (why!)
+  // Adding '/index' since createRequireFromPath('…/') drops trailing slash
   fileURLToPath(new URL(index ? path.replace(/\/([?#].*?|)$/, '/index$1') : path, import.meta.url)),
 );
+const sanitize = path => path.replace(scope, '‹scope›/');
 
 for (const path of paths) {
-  console.group('\ncreateRequireFromPath(%o)\n  .resolve(%o)', path.replace(scope, '‹scope›/'), specifier);
+  console.group('\ncreateRequireFromPath(%o)\n  .resolve(%o)', sanitize(path), specifier);
   try {
-    console.log(
-      `  => %o`,
-      createRequireFromPath(path)
-        .resolve(specifier)
-        .replace(scope, '‹scope›/'),
-    );
+    console.log(`  => %o`, sanitize(createRequireFromPath(path).resolve(specifier)));
   } catch (exception) {
     console.warn(`  => %s`, `${exception}`.split('\n', 1)[0]);
   }
   console.groupEnd();
 }
+
+/*******************************************************************************
+ * @console
+ *
+ * createRequireFromPath('‹scope›/esm/index')
+ *   .resolve('./package.json')
+ *     => '‹scope›/esm/package.json'
+ *
+ * createRequireFromPath('‹scope›/esm/tests/index')
+ *   .resolve('./package.json')
+ *     => '‹scope›/esm/tests/package.json'
+ *
+ ******************************************************************************/
